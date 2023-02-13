92986f07fb75df36669ae189ff5b0565194834e9
Ayu Adiati <retno.ayu98@gmail.com>
2023-02-13 13:07:11 +0100

    Add layout component

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '~/styles/Home.module.css'
 import Link from 'next/link'
-import Navbar from '~/components/Navbar'
-import Footer from '~/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +14,21 @@ export default function Home() {
 				<meta name='description' content="Ayu Adiati's portfolio" />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
-			<Navbar />
 			<main>
 				<h1>Homepage</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia atque dolorum in eaque voluptatem provident voluptate laudantium ullam quaerat minima adipisci sequi unde consequatur, natus necessitatibus qui dolore corrupti? Officia ipsum eum cupiditate nostrum necessitatibus, et placeat minus non at ullam error adipisci perferendis magni laudantium, eveniet commodi laborum, modi in laboriosam itaque aut quia temporibus. Temporibus ea aperiam vero unde ipsam libero saepe ullam provident laudantium? Veritatis, fuga. Cumque!</p>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+					atque dolorum in eaque voluptatem provident voluptate laudantium ullam
+					quaerat minima adipisci sequi unde consequatur, natus necessitatibus
+					qui dolore corrupti? Officia ipsum eum cupiditate nostrum
+					necessitatibus, et placeat minus non at ullam error adipisci
+					perferendis magni laudantium, eveniet commodi laborum, modi in
+					laboriosam itaque aut quia temporibus. Temporibus ea aperiam vero unde
+					ipsam libero saepe ullam provident laudantium? Veritatis, fuga.
+					Cumque!
+				</p>
 				<Link href='/about'>Go to About Me</Link>
 			</main>
-			<Footer />
 		</>
 	)
 }
