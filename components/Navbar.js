fdbc060b9c621c6f37ebcee8af68e3798158a8fc
Ayu Adiati <retno.ayu98@gmail.com>
2023-02-17 15:48:40 +0100

    Install Sass * Style Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styles from '~/styles/Navbar.module.scss'
 
 const Navbar = () => {
 	return (
 		<header>
-			<nav>
+			<nav className={styles.navbar}>
 				<Image
 					src='/images/ayu_adiati_logo.png'
 					height={45}
 					width={45}
 					alt='logo'
+					className={styles.logo}
 				/>
-				<ul>
-					<li>
-						<Link href='/'>Home</Link>
+				<ul className={styles.navList}>
+					<li className={styles.navItem}>
+						<Link href='/' className={styles.navLink}>
+							Home
+						</Link>
 					</li>
-					<li>
-						<Link href='/about'>About Me</Link>
+					<li className={styles.navItem}>
+						<Link href='/about' className={styles.navLink}>
+							About Me
+						</Link>
 					</li>
 				</ul>
 			</nav>
