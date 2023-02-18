ad89d5ec7071b395e988427a9e4ebbbce3c3f568
Ayu Adiati <retno.ayu98@gmail.com>
2023-02-18 12:59:41 +0100

    Add mixin * Basic style  Home and About pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Link from 'next/link'
+import styles from '~/styles/container.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,19 +15,23 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 			</Head>
 			<main>
-				<h1>Homepage</h1>
-				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
-					atque dolorum in eaque voluptatem provident voluptate laudantium ullam
-					quaerat minima adipisci sequi unde consequatur, natus necessitatibus
-					qui dolore corrupti? Officia ipsum eum cupiditate nostrum
-					necessitatibus, et placeat minus non at ullam error adipisci
-					perferendis magni laudantium, eveniet commodi laborum, modi in
-					laboriosam itaque aut quia temporibus. Temporibus ea aperiam vero unde
-					ipsam libero saepe ullam provident laudantium? Veritatis, fuga.
-					Cumque!
-				</p>
-				<Link href='/about'>Go to About Me</Link>
+				<div className={styles.container}>
+					<h1>Homepage</h1>
+					<p className={styles.text}>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia
+						atque dolorum in eaque voluptatem provident voluptate laudantium
+						ullam quaerat minima adipisci sequi unde consequatur, natus
+						necessitatibus qui dolore corrupti? Officia ipsum eum cupiditate
+						nostrum necessitatibus, et placeat minus non at ullam error adipisci
+						perferendis magni laudantium, eveniet commodi laborum, modi in
+						laboriosam itaque aut quia temporibus. Temporibus ea aperiam vero
+						unde ipsam libero saepe ullam provident laudantium? Veritatis, fuga.
+						Cumque!
+					</p>
+					<Link href='/about' className={styles.text}>
+						Go to About Me
+					</Link>
+				</div>
 			</main>
 		</>
 	)
