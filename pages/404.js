d201075584cc0da565f6807a9d2c8662482ade26
Ayu Adiati <retno.ayu98@gmail.com>
2023-02-18 13:02:28 +0100

    Apply container style to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import styles from '~/styles/container.module.scss'
 
 const PageNotFound = () => {
 	const router = useRouter()
@@ -12,13 +13,13 @@ const PageNotFound = () => {
 	}, [router])
 
 	return (
-		<>
+		<div className={styles.container}>
 			<h1>Page Not Found</h1>
 			<h2>You will be redirected back to Homepage.</h2>
 			<h3>
 				Or click <Link href='/'>here</Link> to get back to Homepage.
 			</h3>
-		</>
+		</div>
 	)
 }
 
